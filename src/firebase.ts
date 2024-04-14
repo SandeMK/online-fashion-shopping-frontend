af3076fd229ae1a14addb33302064ba7ea4dbf6b
Makhubela Philasande <47871336+SandeMK@users.noreply.github.com>
2024-04-14 14:48:19 +0200

    feat(chat): hiding secrets in the environment

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = JSON.parse(process.env.FIREBASE_APP_CONFIG || '{}');
+const firebaseConfig = process.env.FIREBASE_APP_CONFIG ?? {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
